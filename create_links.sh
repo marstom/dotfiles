e3880a357bad7df44d147d4e2df27b43f4e9c6ca
Tomasz Marszałek <marstomek@gmail.com>
2018-10-21 17:49:46 +0200

    change vimrc

--- create_links.sh
+++ create_links.sh
@@ -1,5 +1,8 @@
 # create links to this files in home directory
+# vim
 ln -s ~/dotfiles/vim/vimrc_id.symlink ~/.vimrc
-ln -s ~/dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf 
 ln -s ~/dotfiles/vim/vim.symlink ~/.vim
-
+# tmux
+ln -s ~/dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf 
+# visual code settings
+ln -s ~/dotfiles/visual_code/settings.json ~/.config/Code/User/settings.json 
