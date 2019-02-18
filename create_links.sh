28a2b1892296dc1d0256d8242c37d1eb352f20f1
Tomasz Marszałek <tomasz.marszalek@idego.pl>
2019-02-18 17:16:31 +0100

    Add vs code options and some vim plugins, fish as default shell.

--- create_links.sh
+++ create_links.sh
@@ -6,3 +6,6 @@ ln -s ~/dotfiles/vim/vim.symlink ~/.vim
 ln -s ~/dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf 
 # visual code settings
 ln -s ~/dotfiles/visual_code/settings.json ~/.config/Code/User/settings.json 
+
+# vim vundle get latest version
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
