e57de4a7d5dc6baa6febc380ecc640d4cb8a7e51
Tomasz Marszałek <marstomek@gmail.com>
2019-11-10 15:55:52 +0100

    Changes adjusted for home computer.

--- create_links.sh
+++ create_links.sh
@@ -1,6 +1,6 @@
 # create links to this files in home directory
 # vim
-ln -s ~/dotfiles/vim/vimrc_id.symlink ~/.vimrc
+ln -s ~/dotfiles/vim/vimrc.symlink ~/.vimrc
 ln -s ~/dotfiles/vim/vim.symlink ~/.vim
 # tmux
 ln -s ~/dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf 
